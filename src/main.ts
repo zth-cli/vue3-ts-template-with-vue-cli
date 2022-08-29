eda948c6cc5b7ad44be27226342e0897017c58ba
rzx100 <770304867@qq.com>
2022-08-29 15:26:18 +0800

    样式

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import { store } from "./store/index"
+import "element-plus/theme-chalk/dark/css-vars.css"
 import "./styles/index.scss"
 import directives from "./directive"
 // 安装toasts
 import toasts from "./components/Toasts"
 import RegisterIcons from "./icons/index"
-import "element-plus/theme-chalk/dark/css-vars.css"
+
 const app = createApp(App)
 RegisterIcons(app)
 app.use(toasts)
